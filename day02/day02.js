4f5db5b1ee3303d9aaba2ab31533a545beeda622
floj <floj@users.noreply.github.com>
2025-12-02 13:47:25 +0100

    add non-regexp based solution for day2.2

--- day02/day02.js
+++ day02/day02.js
@@ -20,16 +20,39 @@ for (const range of ranges) {
     if (isInvalidIdPart1(i)) {
       sumPart1 += i;
     }
+    // if (isInvalidIdPart2NoRegexp(i)) {
     if (isInvalidIdPart2(i)) {
       sumPart2 += i;
     }
   }
 }
+
 console.log(`Sum Part 1: ${sumPart1}`);
 console.log(`Sum Part 2: ${sumPart2}`);
 
 function isInvalidIdPart2(id) {
   return /^(\d+)(\1)+$/.test(`${id}`);
+}
+
+function isInvalidIdPart2NoRegexp(id) {
+  const numString = `${id}`;
+  if (numString.length < 2) {
+    return false;
+  }
+
+  for (let i = 1; i < numString.length / 2; i++) {
+    const pattern = numString.substring(0, i);
+    for (let j = 0; ; j++) {
+      const check = pattern.repeat(j);
+      if (check.length > numString.length) {
+        break;
+      }
+      if (check === numString) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 function isInvalidIdPart1(id) {
